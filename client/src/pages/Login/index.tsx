59b5de4d48bc6b0e287b690af9d452b5d8fe5ed8
Manjeet_Kumar <manjeetkumar36942336@gmail.com>
2025-04-11 22:06:36 +0530

    fixed some bugs related to routes and animations

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import LogIn from "@/Components/Login/login.jsx";
+import Login from "@/Components/Login/login";
 import Navbar from "@/Components/Navbar/Navbar";
 import Footer from "@/Components/Footer/Footer";
 export default function Home() {
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Navbar />
       <main className="mt-2 sm:mt-6 lg:mt-8">
-        <LogIn />
+        <Login />
       </main>
       <Footer />
     </>
